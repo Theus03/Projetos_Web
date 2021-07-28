053f1ca9dc3f465104c5b4115a36d4eeae5cfea3
Theus03 <matheusssleite@outlook.com>
2021-07-28 12:28:59 -0300

    Create Modal in GameTime in Front-End Project.

---
+++ Front-End/GameTime/script.js
@@ -0,0 +1,21 @@
+// Declarar Variável
+let button_openModal = document.querySelector(".button-add");
+let button_closeModal = document.querySelector(".box-close");
+let modal_overlay = document.querySelector(".modal-overlay");
+
+// Criar Função para o Modal 
+
+const Modal = [  
+    // Abrir Modal  
+    button_openModal.addEventListener("click", () => {
+        modal_overlay.classList.add("active");
+    }),
+
+    //Fechar Modal 
+    button_closeModal.addEventListener("click", () => {
+        modal_overlay.classList.remove("active");
+    })
+]
+
+
+
