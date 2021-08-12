4dd90c0c1b0a3a5e9a3d96c8950c3e123e6b9a56
Theus03 <matheusssleite@outlook.com>
2021-08-12 15:18:24 -0300

    Modal Confrim Add in DreamCity project.

--- Front-End/DreamCity/script.js
+++ Front-End/DreamCity/script.js
@@ -6,5 +6,16 @@ const Modal = {
     modalAddClose() {
         document.querySelector('.modal-overlay.add')
             .classList.remove('active');
+    },
+
+    modalConfAddOpen() {
+        document.querySelector('.modal-overlay.confirmAdd')
+        .classList.add('active');
+        Modal.modalAddClose();
+    },
+
+    modalConfAddClose() {
+        document.querySelector('.modal-overlay.confirmAdd')
+            .classList.remove('active');
     }
 }
