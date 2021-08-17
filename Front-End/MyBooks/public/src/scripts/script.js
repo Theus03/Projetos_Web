90636cf65e13ae782396ad6cd36caa9d1bfc9038
Theus03 <matheusssleite@outlook.com>
2021-08-16 22:43:00 -0300

    Complete insert in MyBooks project front-end

--- Front-End/MyBooks/public/src/scripts/script.js
+++ Front-End/MyBooks/public/src/scripts/script.js
@@ -13,11 +13,11 @@ const Modal = {
 
 const Storage = {
     get() {
-        return JSON.parse(localStorage.getItem("mybooks:books")) || []
+        return JSON.parse(localStorage.getItem("MyBooks: books")) || []
     },
 
     set(books) {
-        localStorage.setItem("mybooks:books", JSON.stringify(books))
+        localStorage.setItem("MyBooks: books", JSON.stringify(books))
     }
 }
 
@@ -48,42 +48,51 @@ const Book = {
 }
 
 const DOM = {
-    bookContainer: document.querySelector('#data-table tbody'),
+    // definindo o container das cidades
+    booksContainer: document.querySelector('#data-table tbody'),
+    
 
-    addBook(book, index) {
+    // Adiciona uma cidade dentro da tabela
+    addBook(book, index){
+        // criar um elemento tr para que possa ser adicionado no html
         const tr = document.createElement('tr');
         tr.innerHTML = DOM.innerHTMLBook(book, index)
         tr.dataset.index = index;
 
-        DOM.bookContainer.appendChild(tr);
+        DOM.booksContainer.appendChild(tr);
     },
 
     innerHTMLBook(book, index) {
+        // estrutura html da tabela
         const html = `
-            <tr>
-                <td class="name_book">${book.name_book}</td>
-                <td class="name_genre">${book.name_genre}</td>
-                <td class="name_author">${book.name_author}</td>
-                <td><img src="../icons/delete.svg" alt="Delete" title="Excluir" class="icon-delete" onclick = "Book.remove(${index})"></td>
-            </tr>
-        `
+        <tr>
+            <td class="name_book">${book.name_book}</td>
+            <td class="name_genre">${book.name_genre}</td>
+            <td class="name_author">${book.name_author}</td>
+            <td><img class="icon-delete" src="../icons/delete.svg" title="Excluir" alt="delete" onclick="Book.remove(${index}) "></td>
+        </tr>
+        ` 
+
         return html
     },
 
     updateTotal() {
-        document.querySelector('.number-result').innerHTML = Book.total();
+        // atualiza o total de cidades
+        document.querySelector('#number-result').innerHTML = Book.total();
     },
 
     clearBooks() {
-        DOM.bookContainer.innerHTML = "";
+        DOM.booksContainer.innerHTML = "";
     }
 }
 
 const Form = {
+    // declarando as informações que tem dentro do formulário
     name_book: document.querySelector('input#name_book'),
     name_genre: document.querySelector('input#name_genre'),
     name_author: document.querySelector('input#name_author'),
 
+    // pegando os valores das caixas de input
     getValues() {
         return {
             name_book: Form.name_book.value,
@@ -92,14 +101,17 @@ const Form = {
         }
     },
 
+    // validando os campos
     validateFields() {
         const { name_book, name_genre, name_author } = Form.getValues()
 
-        if(name_book.trim() === "" || name_genre.trim() === "" || name_author.trim() === "" ){
-            throw new Error("Por favor, o campo nome do livro é obrigatório.")
+        // verifica se os campos estão vazios, caso estejam retornam uma mensagem de erro
+        if(name_book.trim() === "" || name_genre.trim() === "" || name_author.trim() === ""){
+            throw new Error("Por favor, preencha todos os campos.")
         }
     },
 
+    //Formata os valores que estão nos campos
     formatValues() {
         let { name_book, name_genre, name_author } = Form.getValues()
 
@@ -110,12 +122,14 @@ const Form = {
         }
     },
 
+    // limpar campos do formulário
     clearFields() {
         Form.name_book.value = ""
         Form.name_genre.value = ""
         Form.name_author.value = ""
     },
 
+    // aqui ocorre o envio das informações do formulário, assim é chamado a função add para adicionar as informações da cidade na tabela
     submit(event) {
         event.preventDefault()
 
@@ -124,21 +138,23 @@ const Form = {
             const book = Form.formatValues()
             Book.add(book)
             Form.clearFields()
+            location.href = "../home/home.html"
+            Modal.modalSucessOpen();
         } catch (error) {
-            alert(error.Message)
+            alert(error.message)
         }
     }
 }
 
 const App = {
     init() {
-        Book.all.forEach(DOM.add)
+        Book.all.forEach(DOM.addBook)
 
         DOM.updateTotal()
-
+        
         Storage.set(Book.all)
     },
-
+    
     reload() {
         DOM.clearBooks()
         App.init()
