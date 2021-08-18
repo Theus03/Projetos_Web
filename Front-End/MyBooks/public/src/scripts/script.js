5c2508d4dad6d6d22edef4f5fefc578b5d83dde7
Theus03 <matheusssleite@outlook.com>
2021-08-18 00:03:49 -0300

    Complete Modal in MyBooks project front-end

--- Front-End/MyBooks/public/src/scripts/script.js
+++ Front-End/MyBooks/public/src/scripts/script.js
@@ -1,28 +1,25 @@
 const Modal = {
     modalSucessOpen() {
         document.querySelector('.modal-overlay')
-            .classList.add('active');
-        modalSucessClose();
+            .classList.add('active')
     },
     modalSucessClose() {
-        setTimeout((function(){
             document.querySelector('.modal-overlay')
-                .classList.remove('active')
-        }, 1000))
+        .classList.remove('active')
     }
 }
 
 const Back = {
     back() {
         window.history.back();
-        }
+    }
 }
 
 const Storage = {
     get() {
         return JSON.parse(localStorage.getItem("MyBooks: books")) || []
     },
-
+    
     set(books) {
         localStorage.setItem("MyBooks: books", JSON.stringify(books))
     }
@@ -30,26 +27,26 @@ const Storage = {
 
 const Book = {
     all: Storage.get(),
-
+    
     add(book) {
         Book.all.push(book)
-
+        
         App.reload()
     },
-
+    
     remove(index) {
         Book.all.splice(index, 1)
-
+        
         App.reload()
     },
-
+    
     total() {
         let totalBook = 0;
-
+        
         Book.all.forEach(() => {
             totalBook++ 
         });
-
+        
         return totalBook;
     }
 }
@@ -58,97 +55,99 @@ const DOM = {
     // definindo o container das cidades
     booksContainer: document.querySelector('#data-table tbody'),
     
-
+    
     // Adiciona uma cidade dentro da tabela
     addBook(book, index){
         // criar um elemento tr para que possa ser adicionado no html
         const tr = document.createElement('tr');
         tr.innerHTML = DOM.innerHTMLBook(book, index)
         tr.dataset.index = index;
-
+        
         DOM.booksContainer.appendChild(tr);
     },
-
+    
     innerHTMLBook(book, index) {
         // estrutura html da tabela
         const html = `
         <tr>
-            <td class="name_book">${book.name_book}</td>
-            <td class="name_genre">${book.name_genre}</td>
-            <td class="name_author">${book.name_author}</td>
+        <td class="name_book">${book.name_book}</td>
+        <td class="name_genre">${book.name_genre}</td>
+        <td class="name_author">${book.name_author}</td>
             <td><img class="icon-delete" src="../icons/delete.svg" title="Excluir" alt="delete" onclick="Book.remove(${index}) "></td>
-        </tr>
-        ` 
-
-        return html
-    },
-
-    updateTotal() {
-        // atualiza o total de cidades
-        document.querySelector('#number-result').innerHTML = Book.total();
-    },
-
-    clearBooks() {
-        DOM.booksContainer.innerHTML = "";
+            </tr>
+            ` 
+            
+            return html
+        },
+        
+        updateTotal() {
+            // atualiza o total de cidades
+            document.querySelector('#number-result').innerHTML = Book.total();
+        },
+        
+        clearBooks() {
+            DOM.booksContainer.innerHTML = "";
+        }
     }
-}
-
-const Form = {
-    // declarando as informações que tem dentro do formulário
-    name_book: document.querySelector('input#name_book'),
-    name_genre: document.querySelector('input#name_genre'),
-    name_author: document.querySelector('input#name_author'),
-
-    // pegando os valores das caixas de input
-    getValues() {
-        return {
-            name_book: Form.name_book.value,
-            name_genre: Form.name_genre.value,
-            name_author: Form.name_author.value
-        }
-    },
-
-    // validando os campos
-    validateFields() {
-        const { name_book, name_genre, name_author } = Form.getValues()
-
-        // verifica se os campos estão vazios, caso estejam retornam uma mensagem de erro
-        if(name_book.trim() === ""){
-            throw new Error("Por favor, preencha o campo nome, é obrigatório.")
-        }
-    },
-
-    //Formata os valores que estão nos campos
-    formatValues() {
-        let { name_book, name_genre, name_author } = Form.getValues()
-
-        return {
-            name_book,
-            name_genre,
-            name_author
-        }
-    },
-
-    // limpar campos do formulário
-    clearFields() {
-        Form.name_book.value = ""
-        Form.name_genre.value = ""
-        Form.name_author.value = ""
-    },
-
-    // aqui ocorre o envio das informações do formulário, assim é chamado a função add para adicionar as informações da cidade na tabela
-    submit(event) {
-        event.preventDefault()
-
-        try {
-            Form.validateFields()
-            const book = Form.formatValues()
-            Book.add(book)
-            Form.clearFields()
-            Modal.modalSucessOpen()
-            location.href = "../home/home.html"
-        } catch (error) {
-            alert(error.message)
+    
+    const Form = {
+        // declarando as informações que tem dentro do formulário
+        name_book: document.querySelector('input#name_book'),
+        name_genre: document.querySelector('input#name_genre'),
+        name_author: document.querySelector('input#name_author'),
+        
+        // pegando os valores das caixas de input
+        getValues() {
+            return {
+                name_book: Form.name_book.value,
+                name_genre: Form.name_genre.value,
+                name_author: Form.name_author.value
+            }
+        },
+        
+        // validando os campos
+        validateFields() {
+            const { name_book } = Form.getValues()
+            
+            // verifica se os campos estão vazios, caso estejam retornam uma mensagem de erro
+            if(name_book.trim() === ""){
+                throw new Error("Por favor, preencha o campo nome, é obrigatório.")
+            }
+        },
+        
+        //Formata os valores que estão nos campos
+        formatValues() {
+            let { name_book, name_genre, name_author } = Form.getValues()
+            
+            return {
+                name_book,
+                name_genre,
+                name_author
+            }
+        },
+        
+        // limpar campos do formulário
+        clearFields() {
+            Form.name_book.value = ""
+            Form.name_genre.value = ""
+            Form.name_author.value = ""
+        },
+        
+        // aqui ocorre o envio das informações do formulário, assim é chamado a função add para adicionar as informações da cidade na tabela
+        submit(event) {
+            event.preventDefault()
+            
+            try {
+                Form.validateFields()
+                const book = Form.formatValues()
+                Book.add(book)
+                Form.clearFields()
+                Modal.modalSucessOpen()
+                document.querySelector('input').addEventListener('focus', () => {
+                    Modal.modalSucessClose()
+                })
+            } catch (error) {
+                alert(error.message)
         }
     }
 }
@@ -156,7 +155,7 @@ const Form = {
 const App = {
     init() {
         Book.all.forEach(DOM.addBook)
-
+        
         DOM.updateTotal()
         
         Storage.set(Book.all)
