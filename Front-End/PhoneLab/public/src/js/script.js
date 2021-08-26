d1b1b6edd05627dbed560ff5405c550e29e2000c
Theus03 <matheusssleite@outlook.com>
2021-08-25 21:43:09 -0300

    PhoneLab front-end project update.

--- Front-End/PhoneLab/public/src/js/script.js
+++ Front-End/PhoneLab/public/src/js/script.js
@@ -1,7 +1,3 @@
-  
-'use strict'
-
-
 // MODAL
 const Modal = {
     openModal() {
@@ -45,6 +41,9 @@ const createPhone = (phone) => {
     setLocalStorage(dbPhone)
 }
 
+
+// VALIDATION
+
 const isValidFields = () => {
     return document.getElementById('form').reportValidity()
 }
@@ -79,6 +78,9 @@ const savePhone = () => {
     }
 }
 
+
+// DOM
+
 const createRow = (phone, index) => {
     const newRow = document.createElement('tr')
     
@@ -108,6 +110,8 @@ const updateTable = () => {
     clearTable()
     dbPhone.forEach(createRow)
 }
+
+// FORM
 
 const fillFields = (phone) => {
     document.getElementById('modelo').value = phone.modelo
@@ -145,183 +149,11 @@ const editDelete = (event) => {
 
 updateTable()
 
-// Eventos
+
+// EVENTOS
 
 document.getElementById('addPhone')
     .addEventListener('click', savePhone)
 
 document.querySelector('#data-table>tbody')
     .addEventListener('click', editDelete)
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use strict'
-
-// // MODAL
-// const Modal = {
-//     openModal() {
-//         document.querySelector('.modal-overlay').classList.add('active');
-//     },
-//     closeModal() {
-//         document.querySelector('.modal-overlay').classList.remove('active');
-//     }
-// }
-
-// const Storage = {
-//     getStorage() {
-//         JSON.parse(localStorage.getItem('dbPhone')) ?? []
-//     },
-
-//     setStorage(dbPhone) {
-//         localStorage.setItem("dbPhone", JSON.stringify(dbPhone))
-//     }
-// }
-
-// const CRUD = {
-//     delete(index) {
-//         const dbPhone = read()
-//         dbPhone.splice(index, 1)
-//         Storage.setStorage(dbPhone)
-//     },
-
-//     update(index, phone){
-//         const dbPhone = read()
-//         dbPhone[index] = phone
-//         Storage.setStorage(dbPhone)
-//     },
-
-//     read() {
-//         Storage.getStorage()
-//     },
-
-//     create(phone) { 
-//         const dbPhone = Storage.getStorage()
-//         dbPhone.push(phone)
-//         Storage.setStorage(dbPhone)
-//     }
-// }
-
-
-// const isValidFields = () => {
-//     return document.getElementById('form').reportValidity()
-// }
-
-
-
-// const DOM = {
-//     addPhone(phone, index) {
-//         const tr = document.createElement('tr')
-//         tr.innerHTML = `
-//             <td>${phone.modelo}</td>
-//             <td>${phone.marca}</td>
-//             <td>${phone.armazenamento}</td>
-//             <td>${phone.memoria}</td>
-//             <td>${phone.bateria}</td>
-//             <td>
-//                 <div class="container-buttons">
-//                     <input type="button" value="Editar" class="button-edit" id="edit-${index}">
-//                     <input type="button" value="Remover" class="button-remove" id="delete-${index}">
-//                 </div>
-//             </td>
-//         `
-//         document.querySelector('#data-table>tbody').appendChild(tr)
-//     },
-
-//     clearTable() {
-//         const rows = document.querySelectorAll('#data-table>tbody tr')
-//         rows.forEach(row => row.parentNode.removeChild(row))
-//     },
-
-//     updateTable() {
-//         const dbPhone = CRUD.read()
-//         clearTable()
-//         dbPhone.forEach(addPhone)
-//     }
-// }
-
-// const Form = {
-
-//     clearFields() {
-//         const fields = document.querySelectorAll('.modal-field')
-//         fields.forEach(field => field.value = '');
-//         document.getElementById('modelo').dataset.index = 'new'
-//     },
-    
-//     savePhone() {
-//         alert('ou')
-//         if(isValidFields()) {
-//             const phone = {
-//                 modelo: document.getElementById('modelo').value,
-//                 marca: document.getElementById('marca').value,
-//                 armazenamento: document.getElementById('armazenamento').value,
-//                 memoria: document.getElementById('memoria').value,
-//                 bateria: document.getElementById('bateria').value
-//             }
-
-//             const index = document.getElementById('modelo').dataset.index
-            
-//             if(index == 'new') {
-//                 CRUD.create(phone)
-//                 DOM.updateTable()
-//                 Modal.closeModal()
-//             } else {
-//                 CRUD.update(index, phone)
-//                 DOM.updateTable()
-//                 Modal.closeModal()
-//             }
-//         }
-//     },
-
-//     fillFields(phone) {
-//         document.getElementById('modelo').value = phone.modelo
-//         document.getElementById('marca').value = phone.marca
-//         document.getElementById('armazenamento').value = phone.armazenamento
-//         document.getElementById('memoria').value = phone.memoria
-//         document.getElementById('bateria').value = phone.bateria
-//         document.getElementById('modelo').dataset.index = phone.modelo
-//     },
-        
-//     editPhone(index){
-//         const phone = CRUD.read()[index]
-//         phone.index = index
-//         fillFields(phone)
-//         Modal.openModal()
-//     },
-
-//     deletePhone(event){
-//         if (event.target.type == 'button') {
-
-//             const [action, index] = event.target.id.split('-')
-
-//             if (action == 'edit') {
-//                 editPhone(index)
-//             } else {
-//                 const phone = CRUD.read()[index]
-//                 const response = confirm(`Deseja realmente excluir o celular ${phone.nome}`)
-//                 if (response) {
-//                     CRUD.delete(index)
-//                     DOM.updateTable()
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// DOM.updateTable()
-
-
-// // Eventos
-// document.getElementById('add-phone')
-//     .addEventListener('click', Form.savePhone())
-
-// document.querySelector('#data-table>tbody')
-//     .addEventListener('click', editDelete)
