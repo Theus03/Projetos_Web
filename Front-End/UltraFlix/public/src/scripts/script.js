ca294f6915f2ba2d5ed711d7f3370059cda3fcd2
Matheus Souza <54069108+Theus03@users.noreply.github.com>
2021-09-09 13:11:30 -0300

    Add files via upload

--- Front-End/UltraFlix/public/src/scripts/script.js
+++ Front-End/UltraFlix/public/src/scripts/script.js
@@ -90,7 +90,10 @@ const saveFlix = () => {
             if (index == 'new') {
                 createFlix(flix)
                 updateTableFlix()
-            } 
+            } else {
+                updateFlix(index, flix)
+                updateTableFlix()
+            }
         }
     } catch (error) {
         alert(error.message)
@@ -109,10 +112,8 @@ const createRowFlix = (flix, index) => {
     <td>${flix.genero}</td>
     <td>${flix.plataforma}</td>
     <td class="actions-button">
-    <a href="../pages/edit.html" id="edit-${index}">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2
-    .5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-    </a>
+    <svg id="edit-${index}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+    
     <svg id="delete-${index}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon-delete"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
     </td>
     `
@@ -152,6 +153,10 @@ const editDelete = (event) => {
         const[action, index] = event.target.id.split('-')
         
         if(action == 'edit'){
+            document.querySelector('.result-edit').style.display = 'flex'
+            document.querySelector('.container-table').style.display = 'none'
+            document.querySelector('.text-title').style.display = 'none'
+            document.querySelector('.text-title.edit').style.display = 'inline-block'
             editFlix(index)
         } else {
             const flix = readFlix()[index]
