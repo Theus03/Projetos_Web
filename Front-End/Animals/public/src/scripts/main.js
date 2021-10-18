e2e564f84bfd94dc5716d549f8df3c93a1f2d3f9
Theus03 <matheusssleite@outlook.com>
2021-10-18 19:47:30 -0300

    Finish modal delete project Animals.

---
+++ Front-End/Animals/public/src/scripts/main.js
@@ -0,0 +1,10 @@
+// MODAL 
+
+
+function openModal() {
+    document.querySelector(".modal-overlay").classList.add("active");
+}
+
+function closeModal() {
+    document.querySelector(".modal-overlay").classList.remove("active");
+}
